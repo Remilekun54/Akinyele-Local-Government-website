d4e1e03db2fea05086227cc02948485c00e32fbc
Remilekun54 <olaniyanremilekundesmond@gmail.com>
2025-10-27 22:30:41 -0700

    'wsgi'

--- Akinyele/Akinyele/wsgi.py
+++ Akinyele/Akinyele/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Akinyele.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Akinyele.Akinyele.settings')
+
 
 application = get_wsgi_application()
