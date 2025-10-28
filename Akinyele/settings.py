50cbc0631c5f1abb8cb2575ce4c3ba592ce463ca
Remilekun54 <olaniyanremilekundesmond@gmail.com>
2025-10-27 23:23:07 -0700

    'host'

--- Akinyele/settings.py
+++ Akinyele/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-e5lr4b$9#lkguq3gs5i@eq(^bqg#qtm+c)=t5t9f!af9io%12s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'akinyele-local-government-website.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
