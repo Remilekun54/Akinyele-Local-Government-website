2a1db742f07feb7d1b35133b90cd24d5499982ff
Remilekun54 <olaniyanremilekundesmond@gmail.com>
2025-10-27 23:10:40 -0700

    'new'

--- Akinyele/settings.py
+++ Akinyele/settings.py
@@ -38,9 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'contact',
-    'main', 
+    'main',
     'blog',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
